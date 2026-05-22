23cf2b6908ccc9f550f981fc426c1e7766677166
Felt <icebadass@gmail.com>
2026-05-22 15:11:32 +1000

    test

--- public/index.php
+++ public/index.php
@@ -8,10 +8,33 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+$companies = [
+    [
+        "id" => 1,
+        "name" => "Hexlet1",
+        "phone" => "(458) 592-8259"
+    ],
+    [
+        "id" => 2,
+        "name" => "Hexlet2",
+        "phone" => "(458) 592-8259"
+    ],
+    [
+        "id" => 3,
+        "name" => "Hexlet3",
+        "phone" => "(458) 592-8259"
+    ]
+];
+
+$app->get('/companies/{id}', function ($request, $response, array $args) use ($companies) {
+    $id = $args['id'];
+    $collection = collect($companies);
+    $companie = $collection->firstWhere('id', $id);
+
+    if ($companie !== null) {
+        return $response->write(json_encode($companie));
+    }
+    return $response->withStatus(404)->write('Page not found');
 });
+
 $app->run();
